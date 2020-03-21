bfbcd79ca8eafd21b3e95dda356bd7ea4b895777
aryan29 <nk28agra@gmail.com>
2020-03-21 17:54:02 +0530

    Inform.dart

--- app1/lib/src/inform.dart
+++ app1/lib/src/inform.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Inform extends StatefulWidget {
   Inform({Key key}) : super(key: key);
 
@@ -9,8 +10,25 @@ class Inform extends StatefulWidget {
 class _InformState extends State<Inform> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text("gdv"),
+    return Scaffold(
+      appBar: AppBar(title: Text("Inform About Corona Virus Suspects")),
+      body:Container(
+        padding: const EdgeInsets.all(15),
+        color: Theme.of(context).primaryColor,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            FloatingActionButton(onPressed: (){
+              //Request to Get a Person Checked Upon
+            },),
+            FloatingActionButton(onPressed: (){
+              //Request a Distress Signal After taking Readings of Temperature and Other Things
+            },),
+          ],
+          )
+      )
     );
   }
 }

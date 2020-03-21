783c9d14e11156b36970f110044ebeb4678a0861
aryan29 <nk28agra@gmail.com>
2020-03-21 12:57:16 +0530

    Scheduler file

---
+++ app1/try/schedulers.py
@@ -0,0 +1,14 @@
+# This File will be running every 15minutes
+def updating_heat_map():
+    x
+    # Updating Heat map after a regular Interval of Correntine Peoples
+
+
+def send_msg_to_people():
+    x
+    # Send sms to people whose locations is not getting updated
+
+
+def sending_safe_measures():
+    x
+    # Sending safety preacutionary messages to all peoples in our database

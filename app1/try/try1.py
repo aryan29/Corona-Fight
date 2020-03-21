f59e6b8b8d00200abd72fdfec3fc952fdd6f03d5
Ishaan Gupta <ishaang12@gmail.com>
2020-03-21 18:07:40 +0530

    Sat 21 Mar 2020 06:07:40 PM IST

--- app1/try/try1.py
+++ app1/try/try1.py
@@ -3,6 +3,9 @@ known_image = face_recognition.load_image_file("1.jpg")
 unknown_image = face_recognition.load_image_file("7.jpg")
 
 known_encoding = face_recognition.face_encodings(known_image)[0]
+f1 = open("aaa", "w")
+print(known_encoding, file=f1)
+f1.close
 unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
 
 results = face_recognition.compare_faces([known_encoding], unknown_encoding)
